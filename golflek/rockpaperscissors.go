0c947f28ab19a63090a22aa5a47151f86ff9e217
smalldog124 <panumars124@gmail.com>
2018-09-14 10:05:14 +0700

    [Edited]:เพิ่มเทส ค้อนชนะกระดาษ กรรไกรชนะกระดาษ กระดาษชนะค้อน

--- golflek/rockpaperscissors.go
+++ golflek/rockpaperscissors.go
@@ -1,8 +1,35 @@
 package rockpaperscissors
 
 func RockPaperScissors(playerOne, playerTwo string) string {
-	if playerOne == "rock" {
+	if playerOne == "rock" && playerTwo == "scissors" {
 		return "PlayerOneWin"
 	}
+	if playerOne == "rock" && playerTwo == "paper" {
+		return "PlayerTwoWin"
+	}
+	if playerOne == "rock" && playerTwo == "rock" {
+		return "Draw"
+	}
+
+	if playerOne == "scissors" && playerTwo == "paper" {
+		return "PlayerOneWin"
+	}
+	if playerOne == "scissors" && playerTwo == "rock" {
+		return "PlayerTwoWin"
+	}
+	if playerOne == "scissors" && playerTwo == "scissors" {
+		return "Draw"
+	}
+
+	if playerOne == "paper" && playerTwo == "rock" {
+		return "PlayerOneWin"
+	}
+	if playerOne == "paper" && playerTwo == "scissors" {
+		return "PlayerTwoWin"
+	}
+	if playerOne == "paper" && playerTwo == "paper" {
+		return "Draw"
+	}
+
 	return "PlayerTwoWin"
 }
--- golflek/rockpaperscissors_test.go
+++ golflek/rockpaperscissors_test.go
@@ -27,3 +27,37 @@ func Test_RockPaperScissors_Input_PlayerOne_scissors_PlayerTwo_rock_Should_Be_Pl
 		t.Errorf("expect %s but it got %s", expected, actual)
 	}
 }
+func Test_RockPaperScissors_Input_PlayerOne_Scissors_PlayerTwo_Paper_Should_Be_PlayerOneWin(t *testing.T) {
+	expected := "PlayerOneWin"
+	playerOne := "scissors"
+	playerTwo := "paper"
+
+	actual := RockPaperScissors(playerOne, playerTwo)
+
+	if expected != actual {
+		t.Errorf("expect %s but it got %s", expected, actual)
+	}
+}
+
+func Test_RockPaperScissors_Input_PlayerOne_Paper_PlayerTwo_Rock_Should_Be_PlayerOneWin(t *testing.T) {
+	expected := "PlayerOneWin"
+	playerOne := "paper"
+	playerTwo := "rock"
+
+	actual := RockPaperScissors(playerOne, playerTwo)
+
+	if expected != actual {
+		t.Errorf("expect %s but it got %s", expected, actual)
+	}
+}
+func Test_RockPaperScissors_Input_PlayerOne_Rock_PlayerTwo_Rock_Should_Be_Draw(t *testing.T) {
+	expected := "Draw"
+	playerOne := "rock"
+	playerTwo := "rock"
+
+	actual := RockPaperScissors(playerOne, playerTwo)
+
+	if expected != actual {
+		t.Errorf("expect %s but it got %s", expected, actual)
+	}
+}
